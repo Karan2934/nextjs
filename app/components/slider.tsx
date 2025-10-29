444397c4f2a3a9756efeb83bd21513605b9308ab
karan2934 <karan779933@gmail.com>
2025-10-29 13:55:06 +0530

    msg

--- app/components/slider.tsx
+++ app/components/slider.tsx
@@ -1,7 +1,7 @@
 'use client'
 import sliderData from "../../public/jsondata/slider.json";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Autoplay } from 'swiper/modules';
+import { Autoplay, Navigation } from 'swiper/modules';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import 'swiper/css';
@@ -11,20 +11,25 @@ import { faAngleLeft, faAngleRight, faArrowRight, } from "@fortawesome/free-soli
 export default function Slider() {
     return (
         <div className='p-7'>
-            <div className='bg-[url(/icons/pattern-3.jpg)] min-h-64 w-full rounded-3xl'>
+            <div className='bg-[url(/icons/pattern-3.jpg)] min-h-64 w-full rounded-3xl overflow-hidden'>
 
                 <Swiper
+                    modules={[Navigation]}
                     slidesPerView={1}
                     loop={true}
-                    className='flex-1'
+                    navigation={{
+                        prevEl: '.swiper-button-prev',
+                        nextEl: '.swiper-button-next'
+                    }}
+                    className='flex-1 overflow-hidden'
                 >
                     {sliderData.bannerSlider.map((data) => (
-                        <SwiperSlide key={data.id}>
-                            <div className="flex">
+                        <SwiperSlide key={data.id} className="overflow-hidden">
+                            <div className="lg:flex overflow-hidden">
                                 <div className="pt-36 pb-52 pl-12">
-                                    <p className="text-7xl text-white">{data.title1}</p>
-                                    <p className="text-7xl text-white font-bold">{data.title2}</p>
-                                    <p className="text-[16px] text-gray-300">{data.description}</p>
+                                    <p className="text-7xl text-white mb-6">{data.title1}</p>
+                                    <p className="text-7xl text-white font-bold mb-7">{data.title2}</p>
+                                    <p className="text-[16px] text-gray-300 mb-7">{data.description}</p>
                                     <Link href={data.buttonLink}>
                                         <div className=" flex gap-3 items-center hover:">
                                             <button className="bg-gradient-to-r from-[#a051e6f1] to-[#6b43e3] rounded-3xl p-5 font-bold text-white"
@@ -33,41 +38,74 @@ export default function Slider() {
                                         </div>
                                     </Link>
                                 </div>
-                                <div>
-                                    <img src={data.image} className="rounded-l-full w-full h-full" />
+                                <div className="relative">
+
+                                    <div className="">
+                                        <img
+                                            src={data.image}
+                                            alt="AI Marketing"
+                                            className="block w-full lg:ml-60 -mt-19 lg:rounded-full h-auto max-w-full"
+                                        />
+
+                                        <svg
+                                            viewBox="0 0 100 100"
+                                            className="relative w-[179px] h-[179px] rounded-full border border-white/50 bg-white/10 animate-spin-slow text-current origin-center"
+                                        >
+                                        </svg>
+
+                                    </div>
+
+                                    <div className="lg:hidden absolute bottom-4 left-4 bg-[#E6E0FA] w-36 rounded-4xl h-[66px] flex flex-row items-center justify-center gap-3">
+                                        <button className="swiper-button-prev"><FontAwesomeIcon icon={faAngleLeft} className="border border-white p-4 rounded-full text-[#6b43e3]" /></button>
+                                        <button className="swiper-button-next"><FontAwesomeIcon icon={faAngleRight} className="border border-white p-4 rounded-full text-[#6b43e3]" /></button>
+                                    </div>
+
                                 </div>
-                            </div>
-                            <div className="bg-[#E6E0FA] w-36 rounded-4xl h-[66px] flex flex-row items-center justify-center gap-3 m-5">
-                                
-                                <button><FontAwesomeIcon icon={faAngleLeft}  className="border border-white p-4 rounded-full text-[#6b43e3]"/></button>
-                                <button><FontAwesomeIcon icon={faAngleRight} className="border border-white p-4 rounded-full text-[#6b43e3]"/></button>
 
                             </div>
+                            <div className="hidden lg:flex bg-[#E6E0FA] w-36 rounded-4xl h-[66px] flex-row items-center justify-center gap-3 m-5">
+                                <button className="swiper-button-prev"><FontAwesomeIcon icon={faAngleLeft} className="border border-white p-4 rounded-full text-[#6b43e3]" /></button>
+                                <button className="swiper-button-next"><FontAwesomeIcon icon={faAngleRight} className="border border-white p-4 rounded-full text-[#6b43e3]" /></button>
+                            </div>
+
                         </SwiperSlide>
                     ))}
+
                 </Swiper>
             </div>
-            <div className='p-3'>
-                <div className='bg-[#1B152C] rounded-3xl min-h-27 flex items-center p-9'>
-                    <div className='text-gray-100 mr-8'>{sliderData.companySlider.title}</div>
+            <div className='pt-3'>
+                <div className='bg-[#1B152C] rounded-3xl min-h-27 lg:flex-row flex flex-col justify-center items-center p-9'>
+                    <div className='mr-8 pr-14 w-48 lg:w-auto'>
+                        <div className="text-gray-100 font-bold lg:flex-col flex">
+                            <div className="whitespace-nowrap">Trusted By</div>
+                            <div className="whitespace-nowrap">Top Companies</div>
+                        </div>
+                    </div>
 
-                    <Swiper
-                        modules={[Autoplay]}
-                        spaceBetween={20}
-                        slidesPerView={3}
-                        loop={true}
-                        className='flex-1'
-                    >
-                        {sliderData.companySlider.images.map((image, index) => (
-                            <SwiperSlide key={index}>
+
+                    <section className="py-5 overflow-hidden">
+                        <div className="flex animate-marquee gap-20 lg:gap-60">
+                            {/* ---- Original list ---- */}
+                            {sliderData.companySlider.images.map((src, i) => (
                                 <img
-                                    src={image}
-                                    alt={`Client ${index}`}
-                                    className="h-12 w-auto object-contain"
+                                    key={i}
+                                    src={src}
+                                    alt={`Client ${i + 1}`}
+                                    className="h-16 w-auto object-contain flex-shrink-0"
                                 />
-                            </SwiperSlide>
-                        ))}
-                    </Swiper>
+                            ))}
+
+                            {/* ---- Duplicate for seamless loop ---- */}
+                            {sliderData.companySlider.images.map((src, i) => (
+                                <img
+                                    key={`dup-${i}`}
+                                    src={src}
+                                    alt={`Client ${i + 1}`}
+                                    className="h-16 w-auto object-contain flex-shrink-0"
+                                />
+                            ))}
+                        </div>
+                    </section>
                 </div>
             </div>
         </div>
